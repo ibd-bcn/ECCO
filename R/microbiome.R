090d5466dd766cd078ad02379dc5bd6f7bfb6b37
llrs <lluis.revilla@gmail.com>
2019-11-11 16:53:52 +0100

    Check phylum contaminants

---
+++ R/microbiome.R
@@ -0,0 +1,30 @@
+library("dfrtopics") # Github: agoldst/dfrtopics
+library("ggplot2")
+library("dplyr")
+library("scales")
+library("decontam")
+
+microb <- read.csv("~/Downloads/Partek_ECCO_Kraken_Classified_phylum.txt",
+                   sep = "\t", row.names = 1)
+colnames(microb) <- gsub("X[0-9]+_", "", colnames(microb))
+microb <- as.matrix(microb)
+
+prop_microb <- prop.table(microb, 2)
+non_empty <- rowSums(microb)
+m <- dfrtopics::gather_matrix(microb)
+
+isC <- decontam::isContaminant(t(microb), conc = rep(500, ncol(microb)),  method = "frequency")
+
+
+m %>%
+    filter(value != 0) %>%
+    group_by(col_key) %>%
+    mutate(v = value/sum(value)*100) %>%
+    ggplot() +
+    geom_col(aes(col_key, v, fill = row_key)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    # scale_fill_viridis_d() +
+    theme_bw() +
+    theme(axis.text.x = element_blank()) +
+    labs(x = "Samples", y = "%", fill = "Phylum")
+ggsave("plots/percentage_microbiome.png")
