7b26d78e95473a15fb74931b6886a4105b5f33c0
llrs <lluis.revilla@gmail.com>
2019-11-26 17:50:47 +0100

    Looking for the differences

---
+++ R/compare_methods.R
@@ -0,0 +1,24 @@
+# Helper functions to compare
+unlog <- function(x){sign(x)*2^(abs(x))}
+logfc <- function(x){sign(x)*log(abs(x), base = 2)}
+
+
+linear_model <- readRDS("processed/tt_complex.RDS")
+select_samples <- readRDS("processed/juanjo_res.RDS")
+
+linear_model <- linear_model[1:19]
+pdf("plots/compare_techs.pdf")
+for (i in seq_along(linear_model)) {
+    lm_values <- linear_model[[1]]$logFC
+    names(lm_values) <- rownames(linear_model[[i]])
+    s_samples <- logfc(select_samples[[1]][, i])
+    contrasting <- gsub("^fc_", "", colnames(select_samples[[1]]))
+    lm_values <- lm_values[names(s_samples)]
+    plot(lm_values, s_samples, pch = ".",
+         main = paste0("logFC\n", contrasting[i]),
+         xlab = "Linear model", ylab = "Selecting just the samples")
+    print(sum(lm_values == s_samples))
+}
+dev.off()
+
+plot(linear_model[[1]]$AveExpr, linear_model[[1]]$logFC, pch = ".")
